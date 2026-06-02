e092ae4ea71f3b0be45bad7b831fa9f5d1b88dcf
Prince Kumar <princebarnwal030@gmail.com>
2026-06-02 17:07:13 +0530

    Refactor login and signup forms in log.py

--- pages/log.py
+++ pages/log.py
@@ -12,43 +12,45 @@ my = mydb["student"]
 p = st.progress(0)
 
 for i in range(100):
-       p.progress(i+1,"Loading")
-       time.sleep(0.02)
-       p.empty()      
+    p.progress(i+1,"Loading")
+    time.sleep(0.02)
+ 
+p.empty()      
 
 t1,t2=st.tabs(["signin", "signup"])
 with t1:
-       with st.form(key="login_form"):
-              username=st.text_input("Userlogin 👨‍💼")
-              password=st.text_input("🔑 Password ",type="password" )
-              confirm=st.text_input("Confirm Password ", type = "password")              
+    with st.form(key="login_form"):
+        username=st.text_input("Userlogin 👨‍💼")
+        password=st.text_input("🔑 Password ",type="password" )
+        confirm=st.text_input("Confirm Password ", type = "password")              
 
-              st.checkbox("Agree Terms")
-              if st.form_submit_button("Sign In"):
-                     my.insert_one({"userlogin ":username, "password":password})
+        st.checkbox("Agree Terms")
+        if st.form_submit_button("Sign In"):
+            my.insert_one({"userlogin ":username, "password":password})
                      
-                     st.switch_page("pages/contact.py")
-              if password == confirm:
-                     st.success("Thanking for Signup")
-                     st.snow()
+            st.switch_page("pages/contact.py")
+            if password == confirm:
+                st.success("Thanking for Signup")
+                st.snow()
               else:
                   st.error("Your password is incorrect")
 
 with t2:
-       with st.form("SignIn"):
-              t1=st.text_input("👤Username")
-              t2=st.text_input("🔒Password",type="password")
+    with st.form("SignIn"):
+        t1=st.text_input("👤Username")
+        t2=st.text_input("🔒Password",type="password")
               
-              t3=st.date_input("Date of Birth")
-              t4=st.text_input("Mobile Number") 
-              t5=st.radio("Gender",["Female","Male"])
+        t3=st.date_input("Date of Birth")
+        t4=st.text_input("Mobile Number") 
+        t5=st.radio("Gender",["Female","Male"])
                       
-              if st.form_submit_button("SignIn"):
-                  my.insert_one({"username":t1,"password":t2,"dob":str(t3),"mobile number":t4,"Gender":t5"})
-              if not t1 or not t2 or not t3 or not t4 or not t5 :
-                  st.error("Fill The Fields!!!")
-              else:
-                  st.success("Done...")
+        if st.form_submit_button("SignIn"):
+            my.insert_one({"username":t1,"password":t2,"dob":str(t3),"mobile number":t4,"Gender":t5"})
+             
+            if not t1 or not t2 or not t3 or not t4 or not t5 :
+                st.error("Fill The Fields!!!")
+            else:
+                st.success("Done...")
 
 
     
